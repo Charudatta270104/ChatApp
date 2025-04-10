772110d9d3ca9ee93210a1f0d3d066790bca2b5b
Charudatta270104 <charudattawarke098@gmail.com>
2025-04-10 19:47:42 +0530

    fix the user routes errors

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,6 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
@@ -35,7 +34,6 @@ if (process.env.NODE_ENV === "production") {
     res.send("API is running..");
   });
 }
-
 
 // Error Handling middlewares
 app.use(notFound);
@@ -51,7 +49,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3001",
     // credentials: true,
   },
 });
